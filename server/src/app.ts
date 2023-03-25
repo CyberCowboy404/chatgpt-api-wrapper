dc731eee2298a1d369c9b04e605515d1db7112f1
Nick Bohdanov <cybercowboy404@gmail.com>
2023-03-25 20:13:02 +0200

    configure server

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,8 @@ import express, { Application } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import indexRouter from '../routes/index';
-import usersRouter from '../routes/users';
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
 const app: Application = express();
 
