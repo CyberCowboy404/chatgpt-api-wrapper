13344a8ba1fb6362d9033457b65104abc12c8452
Nick Bohdanov <cybercowboy404@gmail.com>
2023-04-07 16:17:45 +0300

    adds generail grid styles

--- client/src/app/chat/chat-page/chat-page.component.ts
+++ client/src/app/chat/chat-page/chat-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-chat-page',
   templateUrl: './chat-page.component.html',
-  styleUrls: ['./chat-page.component.scss']
+  styleUrls: ['./chat-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ChatPageComponent {
 
