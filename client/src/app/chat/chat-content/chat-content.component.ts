630379869b6499d8180e1170fadb64f88e4b5478
Nick Bohdanov <cybercowboy404@gmail.com>
2023-04-08 20:55:10 +0300

    improved chat ui, and now able to ask to chat gpt

--- client/src/app/chat/chat-content/chat-content.component.ts
+++ client/src/app/chat/chat-content/chat-content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from 'chat/chat.service';
 
 @Component({
   selector: 'app-chat-content',
@@ -9,7 +10,7 @@ export class ChatContentComponent {
 
   messages: any[];
 
-  constructor() {
+  constructor(private chatService: ChatService) {
     this.messages = [
       {
         text: "Hello ",
@@ -24,26 +25,50 @@ export class ChatContentComponent {
     ]
   }
 
-  sendMessage(event: any) {
-    const files = !event.files ? [] : event.files.map((file: any) => {
-      return {
-        url: file.src,
-        type: file.type,
-        icon: 'file-text-outline',
-      };
-    });
-
+  sendMessage({ message, files }: { message: string, files: any[] }) {
     this.messages.push({
-      text: event.message,
+      text: message,
       date: new Date(),
       reply: true,
-      type: files.length ? 'file' : 'text',
-      files: files,
+      type: 'text',
       user: {
-        name: 'Jonh Doe',
-        avatar: 'https://i.gifer.com/no.gif',
+        name: 'You',
       },
     });
-    
+
+    this.chatService.completion(message).subscribe((res: any) => {
+      const { data } = res;
+      console.log("🚀 ~ file: chat-content.component.ts:41 ~ ChatContentComponent ~ this.chatService.completion ~ data:", data)
+      this.messages.push({
+        text: data.choices[0].message.content,
+        date: new Date(),
+        reply: false,
+        type: 'text',
+        user: {
+          name: 'Chat GPT',
+        },
+      });
+    });
+
+    // const files = !event.files ? [] : event.files.map((file: any) => {
+    //   return {
+    //     url: file.src,
+    //     type: file.type,
+    //     icon: 'file-text-outline',
+    //   };
+    // });
+
+    // this.messages.push({
+    //   text: event.message,
+    //   date: new Date(),
+    //   reply: true,
+    //   type: files.length ? 'file' : 'text',
+    //   files: files,
+    //   user: {
+    //     name: 'Jonh Doe',
+    //     avatar: 'https://i.gifer.com/no.gif',
+    //   },
+    // });
+
   }
 }
--- client/src/app/core/chat.interceptor.ts
+++ client/src/app/core/chat.interceptor.ts
@@ -15,7 +15,6 @@ export class ChatInterceptor implements HttpInterceptor {
   constructor(private storage: StorageService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log("🚀 ~ file: chat.interceptor.ts:18 ~ ChatInterceptor ~ intercept ~ request:", request)
     if (request.url.includes('/chat')) {
       // Clone the request and attach the specific data
       const authReq = request.clone({
